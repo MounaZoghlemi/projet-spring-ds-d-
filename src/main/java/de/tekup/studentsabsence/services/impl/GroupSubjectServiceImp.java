b02a2006bd63675408d45f6f6d3feb130217da6f
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:56:25 +0100

    Update GroupSubjectServiceImp.java

--- src/main/java/de/tekup/studentsabsence/services/impl/GroupSubjectServiceImp.java
+++ src/main/java/de/tekup/studentsabsence/services/impl/GroupSubjectServiceImp.java
@@ -1,53 +1,53 @@
 package de.tekup.studentsabsence.services.impl;
 
 import de.tekup.studentsabsence.entities.Group;
-import de.tekup.studentsabsence.repositories.GroupRepository;
+import de.tekup.studentsabsence.entities.GroupSubject;
+import de.tekup.studentsabsence.entities.GroupSubjectKey;
+import de.tekup.studentsabsence.entities.Subject;
+import de.tekup.studentsabsence.repositories.GroupSubjectRepository;
 import de.tekup.studentsabsence.services.GroupService;
+import de.tekup.studentsabsence.services.GroupSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
-public class GroupServiceImp implements GroupService {
-    private final GroupRepository groupRepository;
+public class GroupSubjectServiceImp implements GroupSubjectService {
+    private final GroupSubjectRepository groupSubjectRepository;
+    private final GroupService groupService;
 
     @Autowired
-    public GroupServiceImp(GroupRepository groupRepository) {
-        this.groupRepository = groupRepository;
+    public GroupSubjectServiceImp(GroupSubjectRepository groupSubjectRepository,
+                                  GroupService groupService) {
+
+        this.groupSubjectRepository = groupSubjectRepository;
+        this.groupService = groupService;
     }
 
     @Override
-    public List<Group> getAllGroups() {
-        List<Group> groups = new ArrayList<>();
-        groupRepository.findAll().forEach(groups::add);
-        return groups;
+    public void addSubjectToGroup(Group group, Subject subject, float hours) {
+        groupSubjectRepository.save(new GroupSubject(
+                new GroupSubjectKey(group.getId(), subject.getId()),
+                group,
+                subject,
+                hours
+        ));
     }
 
     @Override
-    public Group getGroupById(Long id) {
-        return groupRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No Group With ID: " + id));
+    public List<GroupSubject> getSubjectsByGroupId(Long id) {
+        Group group = groupService.getGroupById(id);
+        return new ArrayList<>(groupSubjectRepository.findAllByGroup(group));
     }
 
     @Override
-    public Group addGroup(Group group) {
-        return groupRepository.save(group);
+    public void deleteSubjectFromGroup(Long gid, Long sid) {
+        //TODO find a groupSubject by Group Id and Subject Id
+        var groupSubject = groupSubjectRepository.findByGroup_IdAndSubject_Id(gid, sid);
+
+        groupSubjectRepository.delete(groupSubject);
     }
 
-    @Override
-    public Group updateGroup(Group group) {
-        if (!groupRepository.existsById(group.getId())) {
-            throw new NoSuchElementException("No Group With ID: " + group.getId());
-        }
-        return groupRepository.save(group);
-    }
-
-    @Override
-    public Group deleteGroup(Long id) {
-        Group group = getGroupById(id);
-        groupRepository.delete(group);
-        return group;
-    }
 }
