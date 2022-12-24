0e2501ab5fb095e1b0dce37c4f5cc00a9b0e8d47
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:50:26 +0100

    Update GroupRepository.java

--- src/main/java/de/tekup/studentsabsence/repositories/GroupRepository.java
+++ src/main/java/de/tekup/studentsabsence/repositories/GroupRepository.java
@@ -2,6 +2,8 @@ package de.tekup.studentsabsence.repositories;
 
 import de.tekup.studentsabsence.entities.Group;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
 }
