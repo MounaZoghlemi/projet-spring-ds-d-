0d42927c5d3729bff1122d3ede0c0e792bdd0e0f
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:26:33 +0100

    Update Group.java

--- src/main/java/de/tekup/studentsabsence/entities/Group.java
+++ src/main/java/de/tekup/studentsabsence/entities/Group.java
@@ -1,36 +1,53 @@
 package de.tekup.studentsabsence.entities;
 
+import de.tekup.studentsabsence.converters.CapitalizeCharConverter;
 import de.tekup.studentsabsence.enums.LevelEnum;
 import de.tekup.studentsabsence.enums.SpecialityEnum;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Set;
+
 @Entity
-@Data
-@ToString(exclude = "students")
+@ToString(exclude = {"students", "subjects"})
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "_group")
+@Table(name = "groups")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id", nullable = false)
     private Long id;
+
     @NotBlank(message = "Name is required")
+    @Column(name = "name", nullable = false, unique = true)
+    @Convert(converter = CapitalizeCharConverter.class)
     private String name;
+
     @NotBlank(message = "Label is required")
+    @Column(name = "label", nullable = false)
+    @Convert(converter = CapitalizeCharConverter.class)
     private String label;
+
+    @NotNull(message = "Level is required")
     @Enumerated(EnumType.STRING)
+    @Column(name = "level", nullable = false)
+    @Convert(converter = CapitalizeCharConverter.class)
     private LevelEnum level;
-    @NotNull(message = "Speciality is required")
+
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "Speciality is required")
+    @Column(name = "speciality", nullable = false)
+    @Convert(converter = CapitalizeCharConverter.class)
     private SpecialityEnum speciality;
-    //TODO Complete Relations with other entities
 
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private Set<Student> students;
 
-
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private Set<GroupSubject> subjects;
 }
