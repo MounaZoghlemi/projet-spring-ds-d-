80b01d87ea0092a71f94b58705f791966ff82d3e
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 21:00:46 +0100

    Update GroupSubjectService.java

--- src/main/java/de/tekup/studentsabsence/services/GroupSubjectService.java
+++ src/main/java/de/tekup/studentsabsence/services/GroupSubjectService.java
@@ -3,11 +3,17 @@ package de.tekup.studentsabsence.services;
 import de.tekup.studentsabsence.entities.Group;
 import de.tekup.studentsabsence.entities.GroupSubject;
 import de.tekup.studentsabsence.entities.Subject;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+
 public interface GroupSubjectService {
     void addSubjectToGroup(Group group, Subject subject, float hours);
+
     List<GroupSubject> getSubjectsByGroupId(Long id);
+
     void deleteSubjectFromGroup(Long gid, Long sid);
 }
+
