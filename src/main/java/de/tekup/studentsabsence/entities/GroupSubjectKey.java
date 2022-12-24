b9d9aa196a3f8fd97950276122f37fb9f32dc0a3
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:38:48 +0100

    Update GroupSubjectKey.java

--- src/main/java/de/tekup/studentsabsence/entities/GroupSubjectKey.java
+++ src/main/java/de/tekup/studentsabsence/entities/GroupSubjectKey.java
@@ -1,19 +1,22 @@
 package de.tekup.studentsabsence.entities;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Table;
 import java.io.Serializable;
+
 @Embeddable
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "group_subject")
 public class GroupSubjectKey implements Serializable {
+
     @Column(name = "group_id")
-    private Long groupId ;
+    private Long groupId;
+
     @Column(name = "subject_id")
     private Long subjectId;
 }
