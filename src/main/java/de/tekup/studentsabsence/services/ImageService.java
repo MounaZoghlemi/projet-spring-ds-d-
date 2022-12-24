98b042b8be069ac0d2c38d3a34e40223f605eb8d
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 21:02:34 +0100

    Update ImageService.java

--- src/main/java/de/tekup/studentsabsence/services/ImageService.java
+++ src/main/java/de/tekup/studentsabsence/services/ImageService.java
@@ -1,12 +1,15 @@
 package de.tekup.studentsabsence.services;
 
 import de.tekup.studentsabsence.entities.Image;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Service
+
 public interface ImageService {
-    Image getImage(String id);
+    Image getImage(Long id);
 
     Image addImage(MultipartFile image) throws IOException;
 }
