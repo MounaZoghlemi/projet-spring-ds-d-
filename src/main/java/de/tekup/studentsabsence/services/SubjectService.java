57762ac391cc002be4a79bf04d5a48ed4bb70b1a
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 21:03:35 +0100

    Update SubjectService.java

--- src/main/java/de/tekup/studentsabsence/services/SubjectService.java
+++ src/main/java/de/tekup/studentsabsence/services/SubjectService.java
@@ -1,8 +1,11 @@
 package de.tekup.studentsabsence.services;
 
 import de.tekup.studentsabsence.entities.Subject;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+@Service
 
 public interface SubjectService {
 
