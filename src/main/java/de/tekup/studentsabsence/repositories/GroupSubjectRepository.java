33a7d9cecdda85091ee4a35cc3f97605e6c0d151
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:49:18 +0100

    Update GroupSubjectRepository.java

--- src/main/java/de/tekup/studentsabsence/repositories/GroupSubjectRepository.java
+++ src/main/java/de/tekup/studentsabsence/repositories/GroupSubjectRepository.java
@@ -4,11 +4,13 @@ import de.tekup.studentsabsence.entities.Group;
 import de.tekup.studentsabsence.entities.GroupSubject;
 import de.tekup.studentsabsence.entities.GroupSubjectKey;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface GroupSubjectRepository extends CrudRepository<GroupSubject, GroupSubjectKey> {
     List<GroupSubject> findAllByGroup(Group id);
-    ///TODO create a methode to find a groupSubject by Group Id and Subject Id
+
+    GroupSubject findByGroup_IdAndSubject_Id(Long group_id, Long subject_id);
 }
