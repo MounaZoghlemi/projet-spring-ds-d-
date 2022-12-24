5df1709467e895f278fbf1c945ebb7c6e2def356
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:42:30 +0100

    Update Subject.java

--- src/main/java/de/tekup/studentsabsence/entities/Subject.java
+++ src/main/java/de/tekup/studentsabsence/entities/Subject.java
@@ -1,25 +1,35 @@
 package de.tekup.studentsabsence.entities;
 
+import de.tekup.studentsabsence.converters.CapitalizeCharConverter;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "subjects")
 public class Subject implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subject_id", nullable = false)
     private Long id;
+
     @NotBlank(message = "Name is required")
+    @Column(name = "name", nullable = false, unique = true)
+    @Convert(converter = CapitalizeCharConverter.class)
     private String name;
+
+    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL)
+    private Set<GroupSubject> groups;
 
 }
