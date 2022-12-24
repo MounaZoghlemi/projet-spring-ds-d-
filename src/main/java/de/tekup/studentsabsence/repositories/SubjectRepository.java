08a6e1459b58a5727b880686b1ac597e11eb2753
MounaZoghlemi <108539984+MounaZoghlemi@users.noreply.github.com>
2022-12-24 20:52:06 +0100

    Update SubjectRepository.java

--- src/main/java/de/tekup/studentsabsence/repositories/SubjectRepository.java
+++ src/main/java/de/tekup/studentsabsence/repositories/SubjectRepository.java
@@ -2,6 +2,9 @@ package de.tekup.studentsabsence.repositories;
 
 import de.tekup.studentsabsence.entities.Subject;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectRepository extends CrudRepository<Subject, Long> {
 }
+
